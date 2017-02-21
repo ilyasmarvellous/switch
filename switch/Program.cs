23470d1b4ca703a3e20d9f259f129cc53b931166
ilyas <ilyas@LAPTOP-NOQ3L7Q3>
2017-02-20 22:17:38 -0600

    Add project files.

---
+++ switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            switch(num)
+            {
+                case 10:
+                    Console.WriteLine("Entered number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Entered number is 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Entered number is 30");
+                    break;
+                default:
+                    Console.WriteLine("Entered number is out of case");
+                    break;
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
